5f0b22b8493d5ce4f6c21f45a94a51eb8d980233
Jon Kagan <darkspadez@gmail.com>
2014-06-22 17:51:15 -0400

    Slim it up
    Change-Id: I36979e645f3911cb82d2446bcd7e1bdf0eaba6e7

---
+++ slim.mk
@@ -0,0 +1,11 @@
+$(call inherit-product, vendor/slim/config/common_full_phone.mk)
+$(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
+
+# Boot animation
+TARGET_SCREEN_WIDTH := 720
+TARGET_SCREEN_HEIGHT := 1280
+
+# Release name
+PRODUCT_NAME := slim_moto_msm8960
+
+$(call inherit-product, device/motorola/moto_msm8960/full_moto_msm8960.mk)
