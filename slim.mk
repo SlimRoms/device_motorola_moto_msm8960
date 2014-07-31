1638085665fa33d63ff987e3994f909212b134e3
Josue Rivera <prbassplayer@gmail.com>
2014-07-31 16:13:03 +0200

    moto_msm8974: Fix strange inherit order.
    Change-Id: Ifd193714ae8db456df4304d34b01880703fe16e2
    Signed-off-by: Josue Rivera <prbassplayer@gmail.com>

--- slim.mk
+++ slim.mk
@@ -5,8 +5,8 @@ $(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
 TARGET_SCREEN_WIDTH := 720
 TARGET_SCREEN_HEIGHT := 1280
 
+$(call inherit-product, device/motorola/moto_msm8960/full_moto_msm8960.mk)
+
 # Release name
 PRODUCT_NAME := slim_moto_msm8960
 PRODUCT_DEVICE := moto_msm8960
-
-$(call inherit-product, device/motorola/moto_msm8960/full_moto_msm8960.mk)
